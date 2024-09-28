f3bdedfa8f52841f04253c13dc6be2d08d33c16e
Sachin Tewatia <tewatiasachin2001@gmail.com>
2024-09-28 22:19:11 +0530

    add routes and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Shared/Navabr/Navbar'
+import Login from './components/Authentication/Login'
+import SignUp from './components/Authentication/SignUp'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+    <Routes>
+      <Route path='/' element={<Navbar/>} ></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/signup' element={<SignUp/>}></Route>
+    </Routes>
     </>
   )
 }
