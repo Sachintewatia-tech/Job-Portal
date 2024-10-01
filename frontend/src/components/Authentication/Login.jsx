9e7cc6ef40fe5eaf6d9c6614bba3cb0d5eb03155
Sachin Tewatia <tewatiasachin2001@gmail.com>
2024-10-01 22:14:39 +0530

    update login-signup

--- frontend/src/components/Authentication/Login.jsx
+++ frontend/src/components/Authentication/Login.jsx
@@ -8,6 +8,9 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { USER_API_ENDPOINT } from '@/Utils/constant'
 import { toast } from 'sonner'
+import { useDispatch, useSelector } from 'react-redux'
+import { setLoading } from '@/Redux/authSlice'
+import { Loader2 } from 'lucide-react'
 
 const Login = () => {
   const [input,setInput] = useState({
@@ -15,28 +18,32 @@ const Login = () => {
     role:"",
     password:""
 });
+const {loading} = useSelector(store=>store.auth);
+
 const navigate = useNavigate();
+const dispatch = useDispatch();
 const changeEvent = (e)=>{
     setInput({...input,[e.target.name]:e.target.value});
 };
 
 const submitHandler = async (e)=>{
   e.preventDefault();
-    try {
+  try {
+    dispatch(setLoading(true))
       const res = await axios.post(`${USER_API_ENDPOINT}/user/login`,input,{
           headers:{
              "Content-Type":"application/json"
           },
           withCredentials:true,
       });
-      console.log('res',res.data);
       if(res.data.success){
           toast.success(res.data.message);
           navigate('/');
       }
   } catch (error) {
-      console.log(error);
       toast.error(error.response.data.message)
+  } finally{
+    dispatch(setLoading(false));
   }
   
 }
@@ -44,7 +51,7 @@ const submitHandler = async (e)=>{
     <div>
       <Navbar/>
       <div className='flex items-center justify-center max-w-7xl mx-auto'>
-        <form onSubmit={submitHandler} className='w-1/3 border border-gray-200 rounded-md p-4 my-10'>
+        <form onSubmit={submitHandler} className='shadow-custom-light w-1/3 border border-gray-200 rounded-md p-4 my-10'>
             <h1 className='font-bold text-xl mb-5'>Login</h1>
 
             <div>
@@ -74,7 +81,12 @@ const submitHandler = async (e)=>{
                 </RadioGroup>
                 
             </div>
-            <Button type='submit' variant='outline' className='w-full my-4 bg-[#007bff] hover:bg-[#0568d1]'>Login</Button>
+            {
+              loading ? 
+              <Button className='w-full my-4' disabled> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait... </Button>
+               :
+              <Button type='submit' variant='outline' className='w-full my-4 bg-[#007bff] hover:bg-[#0568d1]'>Login</Button>
+            }
             <span>Dont't have an account? <Link to={'/signup'}>SignUp</Link> </span>
         </form>
       </div>
---
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+      <h1 className=''>No.1 job hunt website</h1>
+    </div>
+  )
+}
+
+export default HeroSection;
