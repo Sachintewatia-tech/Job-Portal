b6ba7ac3e62cb676e14fbe92dd211d841e41f0b3
Sachin Tewatia <tewatiasachin2001@gmail.com>
2024-10-02 22:35:00 +0530

    add carousel

--- frontend/src/components/HeroSection/HeroSection.jsx
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Button } from '../ui/button';
+import { Search } from 'lucide-react';
 
 const HeroSection = () => {
   return (
-    <div>
-      <h1 className=''>No.1 job hunt website</h1>
+    <div className='text-center'>
+      <div className='flex flex-col gap-5 my-10'>
+        <span className='px-4 py-2 rounded-lg bg-gray-100 text-[#F83002]'>No.1 job hunt website</span>
+        <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Job</span> </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ad, culpa itaque fugiat id est necessitatibus nisi, eaque p voluptate.</p>
+        <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
+          <input type="text" placeholder='find your dream job..' className='outline-none border-none w-full' />
+          <Button className='rounded-r-full bg-[#6A38C2]'> <Search className='h-5 w-5' /> </Button>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from '../Shared/Navabr/Navbar'
+import HeroSection from '../HeroSection/HeroSection'
+import CategoryCrausal from '../CategoryCrausal/CategoryCrausal'
+import LatestJob from '../LatestJob/LatestJob'
 
 const Home = () => {
   return (
     <div>
-        <Navbar/>
-      HOME
+      <Navbar/>
+      <HeroSection/>
+      <CategoryCrausal/>
+      <LatestJob/>
     </div>
   )
 }
